0a8323db023146c75feeb1909d2ebc518cbb53df
oselumeseagbonrofo <113614713+oselumeseagbonrofo@users.noreply.github.com>
2023-09-25 21:10:53 +0100

    book donations app created

---
+++ BookDonations/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BookdonationsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "BookDonations"
---
+++ BookDonations/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Book_Donor(models.Model):
+    SCHOOL_CLASS_LEVEL_CHOICES = [
+        ("KG", "Kindagarten"),
+        ("NUR", "Nursery"),
+        ("PRY", "Primary"),
+        ("JSS", "Junior Secondary School"),
+        ("SSS", "Senior Secondary School"),
+        ("UNI", "University"),
+    ]
+    school_class_level = models.CharField(
+        max_length=3,
+        choices=SCHOOL_CLASS_LEVEL_CHOICES,
+        default = "KG",
+        db_index=True
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    book_amount = models.IntegerField()
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField()
+    def __str__(self):
+        name = self.first_name + " " + self.last_name
+        return name
+  
+class Book_Receiver(models.Model):
+    SCHOOL_CLASS_LEVEL_CHOICES = [
+        ("KG", "Kindagarten"),
+        ("NUR", "Nursery"),
+        ("PRY", "Primary"),
+        ("JSS", "Junior Secondary School"),
+        ("SSS", "Senior Secondary School"),
+        ("UNI", "University"),
+    ]
+    school_class_level = models.CharField(
+        max_length=3,
+        choices=SCHOOL_CLASS_LEVEL_CHOICES,
+        default = "KG",
+        db_index=True
+    )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    book_amount = models.IntegerField()
+    phone_number = models.CharField(max_length=11)
+    email = models.EmailField()
+    def __str__(self):
+        name = self.first_name + " " + self.last_name
+        return name
